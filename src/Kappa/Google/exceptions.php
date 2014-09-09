8b9a2f46d0d70607f697334430182d51f7de0897
Budry <zarubaondra@gmail.com>
2014-09-09 16:17:54 +0200

    Initial commit

---
+++ src/Kappa/Google/exceptions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the google package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\Google;
